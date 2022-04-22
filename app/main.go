1a6479f31f7c2372ea7c7499cd941e58c74356e7
joshuaomonemu <joshuaomonemu@gmail.com>
2022-04-22 09:52:08 +0100

    testing paystack API

--- app/main.go
+++ app/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"app/routes"
-	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
+type User struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
 func main() {
-	fmt.Println("Starting.....")
-	routes.Start()
+	resp, err := http.Get("https://api.paystack.co/bank?Authorization=Bearer YOUR_SECRET_KEY")
+
+	if err != nil {
+		log.Printf("Request Failed: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Reading body failed: %s", err)
+		return
+	}
+	// Log the request body
+	bodyString := string(body)
+	log.Print(bodyString)
 }
